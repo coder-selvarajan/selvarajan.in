80a1ea95fd9afc21d6501e2aa3f41b7698c6ff40
Selvarajan Thangavel <selvarajan2006@gmail.com>
2025-01-06 10:39:17 +0530

    Added "Talks" section on the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <main className="max-w-[1024px] mx-auto px-4">
-      <section className="py-8 md:py-24 flex flex-col md:flex-row items-center gap-12">
+      <section className="py-8 md:py-20 flex flex-col md:flex-row items-center gap-12">
         {/* Hero Content */}
-        <div className="w-full md:w-1/2 space-y-6 pl-4">
+        <div className="w-full md:w-3/5 space-y-6 pl-4 md:pr-8">
           <h1 className="text-5xl font-quicksand font-bold leading-tight">
             Hi, I'm <span className="text-orange-600">Selvarajan</span>
           </h1>
@@ -15,17 +15,7 @@ const Home = () => {
             My journey blends creativity with technology, crafting digital experiences that resonate with both beauty and purpose.
           </p>
           
-          <div className="pt-6 flex gap-4">
-            <a 
-              href="https://github.com/coder-selvarajan" 
-              target="_blank"
-              className="inline-flex items-center gap-2 bg-gray-300 px-5 py-3 rounded-lg hover:text-orange-600 transition-colors"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
-              </svg>
-              GitHub
-            </a>
+          <div className="pt-6 flex gap-5 pb-10">
             <a
               href="https://www.linkedin.com/in/selvarajan-t"
               target="_blank" 
@@ -36,12 +26,23 @@ const Home = () => {
               </svg>
               LinkedIn
             </a>
+
+            <a
+              href="https://www.youtube.com/@alchemyofcode"
+              target="_blank" 
+              className="inline-flex items-center gap-2 bg-gray-300 px-5 py-3 rounded-lg hover:text-orange-600 transition-colors"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M23.498 6.186a3.001 3.001 0 00-2.111-2.123C19.5 3.5 12 3.5 12 3.5s-7.5 0-9.387.563a3.001 3.001 0 00-2.111 2.123C0 8.064 0 12 0 12s0 3.936.502 5.814a3.001 3.001 0 002.111 2.123C4.5 20.5 12 20.5 12 20.5s7.5 0 9.387-.563a3.001 3.001 0 002.111-2.123C24 15.936 24 12 24 12s0-3.936-.502-5.814zM9.75 15.25v-6.5L16.25 12l-6.5 3.25z"/>
+              </svg>
+              YouTube
+            </a>
           </div>
         </div>
         
         {/* Hero Image */}
-        <div className="w-full md:w-1/2">
-          <div className="relative w-full max-w-[350px] mx-auto mb-8">
+        <div className="w-full md:w-2/5 md:align-bottom ">
+          <div className="relative w-full max-w-[350px] mb-8 mx-auto">
             <img 
               src="/images/me.png"
               alt="Developer Color" 
@@ -54,7 +55,7 @@ const Home = () => {
             />
           </div>
 
-          <div className="flex justify-center gap-8 text-gray-600">
+          <div className="flex justify-center gap-8 text-gray-600 ">
             <div className="flex items-center gap-2">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
@@ -77,9 +78,10 @@ const Home = () => {
         </div>
       </section>
 
+
       {/* Projects Section */}
       <section className="py-12">
-        <h2 className="text-4xl font-quicksand font-bold text-center mb-12">My Apps</h2>
+        <h2 className="text-4xl font-quicksand font-bold text-center mb-12">Apps</h2>
         <div className="grid grid-cols-1 gap-12">
           {/* Full-width Tamil Lit Box */}
           <Link to="/tamil-lit" className="group" onClick={() => window.scrollTo(0, 0)}>
@@ -214,11 +216,51 @@ const Home = () => {
             </Link>
           </div>
         </div>
-        
+      </section>
+
+      {/* My Talks Section */}
+      <section className="py-12">
+        <h2 className="text-4xl font-quicksand font-bold text-center mb-8">Talks</h2>
+        <div className="bg-gray-200 rounded-2xl shadow-lg overflow-hidden max-w-[1024px] mx-auto p-6 md:p-10">
+          <div className="flex flex-col md:flex-row items-center gap-8">
+            {/* Embedded Video */}
+            <div className="w-full md:w-1/2">
+              <div className="relative pb-[56%] h-0">
+                <iframe
+                  className="absolute top-0 left-0 w-full h-full rounded-lg shadow-lg"
+                  src="https://www.youtube.com/embed/EDakjMVUlSk"
+                  title="SwiftUI Animations Session"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
+            {/* Description */}
+            <div className="w-full md:w-1/2 text-center md:text-left">
+              <p className="text-lg text-gray-600 mb-6">
+                Feeling incredibly grateful to have delivered my first-ever online session on SwiftUI animations with such an inspiring iOS Developer community! 🙌 
+                <br/><br/>
+                It’s amazing to see how SwiftUI continues to open up endless possibilities for creating delightful user experiences. 🚀 
+              </p>
+              <a
+                href="https://www.youtube.com/watch?v=EDakjMVUlSk"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex items-center gap-3 rounded-lg  hover:text-orange-600 transition-shadow"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-8 h-8" viewBox="0 0 24 24">
+                  <path d="M23.498 6.186a3.001 3.001 0 00-2.111-2.123C19.5 3.5 12 3.5 12 3.5s-7.5 0-9.387.563a3.001 3.001 0 00-2.111 2.123C0 8.064 0 12 0 12s0 3.936.502 5.814a3.001 3.001 0 002.111 2.123C4.5 20.5 12 20.5 12 20.5s7.5 0 9.387-.563a3.001 3.001 0 002.111-2.123C24 15.936 24 12 24 12s0-3.936-.502-5.814zM9.75 15.25v-6.5L16.25 12l-6.5 3.25z"/>
+                </svg>
+                <span className="text-lg font-semibold">Watch on YouTube</span>
+              </a>
+            </div>
+          </div>
+        </div>
       </section>
 
       <section className="py-4 md:py-16">
-        <h2 className="text-4xl font-quicksand font-bold mb-4 text-center">My Recent Artworks</h2>
+        <h2 className="text-4xl font-quicksand font-bold mb-4 text-center">Recent Artworks</h2>
         {/* <p className="text-center text-gray-600 mb-12">Goto <a href="https://art.selvarajan.in" target="_blank" className="text-orange-600 hover:text-orange-700">art.selvarajan.in</a> to view my full portfolio</p> */}
         <div className="text-center mb-10">
           <a 
@@ -343,6 +385,11 @@ const Home = () => {
               <a href="https://twitter.com/selvarajant" className="hover:text-orange-600" target="_blank" rel="noopener noreferrer">
                 <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/>
+                </svg>
+              </a>
+              <a href="https://www.youtube.com/@alchemyofcode" className="hover:text-orange-600" target="_blank" rel="noopener noreferrer">
+                <svg className="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M23.498 6.186a3.001 3.001 0 00-2.111-2.123C19.5 3.5 12 3.5 12 3.5s-7.5 0-9.387.563a3.001 3.001 0 00-2.111 2.123C0 8.064 0 12 0 12s0 3.936.502 5.814a3.001 3.001 0 002.111 2.123C4.5 20.5 12 20.5 12 20.5s7.5 0 9.387-.563a3.001 3.001 0 002.111-2.123C24 15.936 24 12 24 12s0-3.936-.502-5.814zM9.75 15.25v-6.5L16.25 12l-6.5 3.25z"/>
                 </svg>
               </a>
             </div>
